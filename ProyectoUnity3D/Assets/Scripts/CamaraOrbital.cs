1981c864d8236eaa6066121b6e9ab83aed2dfcf6
MamadiMD <mmagassdam1@educacion.navarra.es>
2025-11-20 22:25:57 +0100

    segundo mapa completo

--- ProyectoUnity3D/Assets/Scripts/CamaraOrbital.cs
+++ ProyectoUnity3D/Assets/Scripts/CamaraOrbital.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class CamaraOrbital : MonoBehaviour
 {
-    public Transform target;       
+    public Transform target;      
     public float distance = 5f;    
     public float sensitivityX = 200f;
     public float sensitivityY = 150f;
-    public float minY = -20f;      
+    public float minY = -20f;
     public float maxY = 70f;
 
     private float rotX;
@@ -18,10 +18,9 @@ public class CamaraOrbital : MonoBehaviour
     {
         if (target == null)
         {
-            Debug.LogError("No se asignÛ un target en la c·mara orbital.");
+            Debug.LogError("No se asign√≥ un target en la c√°mara orbital.");
         }
 
-        
         Vector3 angles = transform.eulerAngles;
         rotX = angles.y;
         rotY = angles.x;
@@ -31,21 +30,19 @@ public class CamaraOrbital : MonoBehaviour
     {
         if (target == null) return;
 
-        
-        rotX += Input.GetAxis("Mouse X") * sensitivityX * Time.deltaTime;
-        rotY -= Input.GetAxis("Mouse Y") * sensitivityY * Time.deltaTime;
+        if (Input.GetMouseButton(2))
+        {
+            rotX += Input.GetAxis("Mouse X") * sensitivityX * Time.deltaTime;
+            rotY -= Input.GetAxis("Mouse Y") * sensitivityY * Time.deltaTime;
 
-        
-        rotY = Mathf.Clamp(rotY, minY, maxY);
+            rotY = Mathf.Clamp(rotY, minY, maxY);
+        }
 
-        
         Quaternion rotation = Quaternion.Euler(rotY, rotX, 0);
 
-        
         Vector3 direction = rotation * new Vector3(0, 0, -distance);
         transform.position = target.position + direction;
 
-        
         transform.LookAt(target);
     }
 }
